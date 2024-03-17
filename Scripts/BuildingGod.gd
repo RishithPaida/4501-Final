ce1ed5553993a756af14dddc967e2e3ed62cef23
RishithPaida <rishithpaida@gmail.com>
2024-03-16 20:48:42 -0400

    building god change

--- Scripts/BuildingGod.gd
+++ Scripts/BuildingGod.gd
@@ -34,13 +34,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if God.Curr_State == God.State.Build:
-		
+			
 		var camera = get_viewport().get_camera_3d()
 		var from = camera.project_ray_origin(get_viewport().get_mouse_position())
 		var to = from + camera.project_ray_normal(get_viewport().get_mouse_position()) * 1000
-		var mouse_pos = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, to)
-		Curr_Selected_Building_To_Build.position = Vector3(round(mouse_pos.x),round(mouse_pos.y),round(mouse_pos.z))
-		
+
+		#var mouse_pos = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, to)
+		#Curr_Selected_Building_To_Build.position = Vector3(round(mouse_pos.x),round(mouse_pos.y),round(mouse_pos.z))
+
+		var space_state = get_world_3d().direct_space_state
+		var query = PhysicsRayQueryParameters3D.create(from, to)
+		query.exclude = [self]
+		query.collision_mask = 2
+		var result = space_state.intersect_ray(query)
+
+		if(result):
+			Curr_Selected_Building_To_Build.position = result.position
 		
 		if can_build:
 			Curr_Selected_Building_To_Build.get_node("MeshInstance3D").material_override = green_shade
