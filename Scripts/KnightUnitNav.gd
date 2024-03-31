f5041bfac445691bdcdae1227d490470c7cfede2
Rhys <greenskittle126miner@gmail.com>
2024-03-31 12:08:38 -0400

    created KnightNav script
    Haven't set it up yet though

--- Scripts/KnightUnitNav.gd
+++ Scripts/KnightUnitNav.gd
@@ -3,16 +3,16 @@ extends CharacterBody3D
 enum Task{
 	Idle,
 	Selected,
-	GettingResources,
+	Attacking,
 	Walking,
-	Delivering
 }
 
 var currentTask = Task.Idle
 var resourcesHolding = 0
 var Home
 
-var harvestUnit
+
+var target
 var runOnce = true
 @export var speed = 2
 
@@ -27,34 +27,15 @@ func _ready() -> void:
 func _physics_process(delta):
 	
 	if not is_on_floor():
+		
 		velocity.y -= gravity * delta
 		move_and_slide()
 		
 	match currentTask:
 		Task.Idle:
 			pass
-		Task.Delivering:
-			if(global_position.distance_to(Home) > 1):
-				#print(position.distance_to(Home.global_position))
-				walk()
-			else:
-				print("Dropped off!")
-				God.ruby += resourcesHolding
-				resourcesHolding = 0
-				harvest(harvestUnit)
-		Task.GettingResources:
-			if(position.distance_to(harvestUnit.global_position) > 2):
-				#print(position.distance_to(harvestUnit.global_position))
-				walk()
-			else:
-				if runOnce:
-					runOnce = false
-					await get_tree().create_timer(2).timeout
-					runOnce = true
-					resourcesHolding = 10
-					moveTo(Home)
-					currentTask = Task.Delivering
-					print("Harvested!")
+		Task.Attacking:
+			pass
 		Task.Walking:
 			if(navAgent.is_navigation_finished()):
 				Task.Idle
@@ -67,26 +48,16 @@ func moveTo(pos : Vector3):
 	currentTask = Task.Walking
 	navAgent.set_target_position(pos)
 	
-func harvest(resource):
-	harvestUnit = resource
-	moveTo(harvestUnit.global_position)
-	currentTask = Task.GettingResources
-	print("Going to harvest!")
-	
+
 func walk():
 	var targetPos = navAgent.get_next_path_position()
 	var direction = global_position.direction_to(targetPos)
 	velocity = direction * speed
-	
 	move_and_slide()
 	
-	
 
-func setDeliver():
-	if(resourcesHolding == 0):
-		return
-	moveTo(Home)
-	currentTask = Task.Delivering
+func attack(enemy):
+	target = enemy
 
 func select():
 	$SelectionCircle.show()
